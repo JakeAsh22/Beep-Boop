ff1f59a559ced50d457223411f56936ac1f5c1da
JakeAsh22 <jacob.ash1998@gmail.com>
2019-11-01 16:50:37 -0700

    Will clean up code, make the website able to be used more than once

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,9 @@ function listMaker (input)
     if(userArray[i].includes(3))
       userArray[i]="I'm sorry Dave, I'm afraid I can't do that.";
     else if(userArray[i].includes(2))
-        userArray[i]="Boop!";
+      userArray[i]="Boop!";
     else if(userArray[i].includes(1))
-          userArray[i]="Beep!";
+      userArray[i]="Beep!";
   }
 
   console.log(userArray);
@@ -21,6 +21,8 @@ $(document).ready(function()
     $("form#formBeep").submit(function(event)
   {
     event.preventDefault();
+    userArray=[];
+    $("#output").text("");
     var userInput = parseInt($("input#input").val());
     listMaker(userInput);
     $("#results").show();
