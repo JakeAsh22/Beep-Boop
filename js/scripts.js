adc595596ee47822b21fb145dcc6d7602b491211
JakeAsh22 <jacob.ash1998@gmail.com>
2019-11-01 16:28:55 -0700

    Will create the website, uses for loops and uses .includes method to check if the number has a 3, 2, or 1.

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var userArray=[];
+function listMaker (input)
+{
+  for (var i = 0;i<=input;i++)
+  {
+    userArray[i]=i.toString();
+    if(userArray[i].includes(3))
+      userArray[i]="I'm sorry Dave, I'm afraid I can't do that.";
+    else if(userArray[i].includes(2))
+        userArray[i]="Boop!";
+    else if(userArray[i].includes(1))
+          userArray[i]="Beep!";
+  }
+
+  console.log(userArray);
+}
+
+
+$(document).ready(function()
+{
+    $("form#formBeep").submit(function(event)
+  {
+    event.preventDefault();
+    var userInput = parseInt($("input#input").val());
+    listMaker(userInput);
+    $("#results").show();
+    for(var i=0;i<=userArray.length;i++)
+    {
+      $("#output").append(userArray[i]);
+      $("#output").append(" ");
+    }
+
+
+  })
+});
